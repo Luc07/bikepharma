70852916db2bed6bafac8f8777d6f2054f4a11a1
unknown <lucas1822@outlook.com>
2024-11-05 14:36:59 -0300

    tema no layout e pt-BR

--- web/app/layout.js
+++ web/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
